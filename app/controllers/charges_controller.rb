a9a8ddfd4644120b723e844e58e1c4e1b249346a
Victor Velazquez <velazquezgaspar16@hotmail.com>
2018-03-07 19:05:05 -0600

    Add dynamic amount charge

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,4 +1,6 @@
 class ChargesController < ApplicationController
+  before_action :amount_to_be_charged
+
   def new
   end
 
@@ -15,5 +17,11 @@ class ChargesController < ApplicationController
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
+  end
+
+  private
+
+  def amount_to_be_charged
+    @amount = 500
   end
 end
